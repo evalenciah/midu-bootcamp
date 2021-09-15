c43bbb6646c162f52ac23f28820ca1ec16248005
Emanuel Valencia <evalenciah@unal.edu.co>
2021-09-15 11:13:11 -0500

    Creación del repositorio

---
+++ part1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+
+  const mensaje = 'Hola mundo desde variable'
+
+  return (
+    <div className="App">
+      {mensaje}
+    </div>
+  );
+
+}
+
+export default App;
