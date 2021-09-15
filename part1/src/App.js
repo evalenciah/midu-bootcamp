9c0b31fce65aa6049ab7c73bf803fbe49e4ccaff
Emanuel Valencia <evalenciah@unal.edu.co>
2021-09-15 11:40:53 -0500

    Ex 1.1 - 1.2: courseinfo

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,15 +1,49 @@
-import './App.css';
+import React from 'react'
 
-const App = () => {
+const Part = ({part}) => <p>{part.name} {part.exercises}</p>
 
-  const mensaje = 'Hola mundo desde variable'
+const Header = ({course}) => <h1>{course}</h1>
 
+const Content = ({parts}) => {
   return (
-    <div className="App">
-      {mensaje}
+    <div>
+      <Part part={parts[0]}/>
+      <Part part={parts[1]}/>
+      <Part part={parts[2]}/>
     </div>
-  );
-
+  )
 }
 
-export default App;
+const Total = ({parts}) => {
+  return (
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      }, {
+        name: 'Using props to pass data',
+        exercises: 7
+      }, {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App
